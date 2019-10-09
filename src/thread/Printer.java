b61f2128ebc6ecca5935b60aeda7f825f2249014
Jacopo Famà4 <famajacopo.01@gmail.com>
2019-10-09 11:48:06 +0200

    Printer class modified, added run

--- src/thread/Printer.java
+++ src/thread/Printer.java
@@ -2,5 +2,18 @@ package thread;
 
 public class Printer implements Runnable
 {
+    Storage stg = new Storage();
+    public int Indice;
     
+    Printer(Storage stg)//Inizializzazione stg
+    {
+        this.stg = stg;
+    }
+    
+    @Override
+    public void run()//Classe run che legge la variabile in Storage
+    {
+       Indice = stg.getiIndice(); 
+       System.out.println("Indice: " + Indice);
+    }
 }
