ce7830c5bcabb426ac0e0607bb26670ac831c6ef
Jacopo Famà4 <famajacopo.01@gmail.com>
2019-10-09 12:07:17 +0200

    Printer class modified, added comments

--- src/thread/Printer.java
+++ src/thread/Printer.java
@@ -12,12 +12,12 @@ public class Printer implements Runnable
     @Override
     public void run()//Classe run che legge la variabile in Storage
     {      
-       int i;
+        int i;//Inizializzazione variabile contatore
         
-        for(i=0; i<50; i++)
+        for(i=0; i<50; i++)//Ciclo per mettere a video il risultato
         {
-           Numero = Storage.getiNumero();
-           System.out.println("Numero: " + Numero);
+           Numero = Storage.getiNumero();//Chiamata che prende il numero da storage
+           System.out.println("Numero: " + Numero);//Print del numero
         }
     }
 }
